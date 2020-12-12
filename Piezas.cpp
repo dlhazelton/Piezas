3acf1f997fbfa32f3d84b407e6a2b597e7baf671
dlhazelton <dlhazelton@mail.csuchico.edu>
2020-12-11 23:27:14 -0800

    Submission for Quiz, getState not finished

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,13 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  board[BOARD_ROWS][BOARD_COLS] = {Blank};
+  turn = X;
 }
 
 /**
@@ -30,19 +32,108 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  for(int i = 0; i < BOARD_ROWS; i++) {
+    for(int j = 0; j < BOARD_COLS; j++) {
+      board[i][j] = Blank;
+    }
+  }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  // O's turn
+  if(turn ==  O) {
+    if (column >= 0 || column < 4) {
+
+      // Checking Row 0
+      if (board[0][column] == Blank) {
+        board[0][column] = O;
+        turn = X;
+        return O;
+      }
+
+      // Checking Row 1
+      else {
+        if (board[1][column] == Blank) {
+          board[1][column] = O;
+          turn = X;
+          return O;
+        }
+
+        // Checking Row 2
+        else {
+          if (board[2][column] == Blank) {
+            board[2][column] = O;
+            turn = X;
+            return O;
+          }
+
+          // All rows filled, wasted turn
+          else {
+            turn = X;
+            return Blank;
+          }
+        }
+      }
+    }
+
+    // Turn is out of bounds
+    else {
+      turn = X;
+      return Invalid;
+    }
+  }
+
+  // X's turn
+  else {
+    if (column >= 0 || column < 4) {
+
+      // Checking Row 0
+      if (board[0][column] == Blank) {
+        board[0][column] = X;
+        turn = O;
+        return X;
+      }
+
+      // Checking Row 1
+      else {
+        if (board[1][column] == Blank) {
+          board[1][column] = X;
+          turn = O;
+          return X;
+        }
+
+        // Checking Row 2
+        else {
+          if (board[2][column] == Blank) {
+            board[2][column] = X;
+            turn = O;
+            return X;
+          }
+
+          // All rows filled, wasted turn
+          else {
+            turn = O;
+            return Blank;
+          }
+        }
+      }
+    }
+
+    // Turn is out of bounds
+    else {
+      turn = O;
+      return Invalid;
+    }
+  }
 }
 
 /**
@@ -51,13 +142,18 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+  if (row < BOARD_ROWS && column < BOARD_COLS) {
+    return board[row][column];
+  }
+  else {
+    return Invalid;
+  }
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,87 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+// What an NSane dummy test you got here
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+// Testing constructor & reset
+TEST(PiezasTest, constructTest)
+{
+  Piezas();
+  Piece dummyPiece = board[0][0];
+  ASSERT_EQ(dummyPiece, Blank);
+}
+
+TEST(PiezasTest, resetTest)
+{
+  reset();
+  Piece dummyPiece = board[0][0];
+  ASSERT_EQ(dummyPiece, Blank);
+}
+
+// Testing dropPiece
+TEST(PiezasTest, validEmptyDrop)
+{
+  dropPiece(0);
+  Piece dummyPiece = board[0][0];
+  ASSERT_NE(dummyPiece, Blank);
+}
+
+TEST(PiezasTest, filledColumnDrop)
+{
+  dropPiece(0);
+  dropPiece(0);
+  dropPiece(0);
+  Piece dummyPiece = dropPiece(0);
+  ASSERT_EQ(dummyPiece, Blank);
+}
+
+TEST(PiezasTest, invalidDrop)
+{
+  Piece dummyPiece = dropPiece(4);
+  ASSERT_NE(dummyPiece, Invalid);
+}
+
+TEST(PiezasTest, errorDrop) {
+  ASSERT_NO_THROW(dropPiece("A"));
+}
+
+// Testing pieceAt
+TEST(PiezasTest, validAt)
+{
+  dropPiece(0);
+  Piece dummyPiece = pieceAt(0, 0);
+  ASSERT_NE(dummyPiece, Blank);
+}
+
+TEST(PiezasTest, emptyAt)
+{
+  Piece dummyPiece = pieceAt(0, 0);
+  ASSERT_EQ(dummyPiece, Blank);
+}
+
+TEST(PiezasTest, invalidAt)
+{
+  Piece dummyPiece = pieceAt(4, 0);
+  ASSERT_EQ(dummyPiece, Invalid);
+}
+
+TEST(PiezasTest, errorAt)
+{
+  ASSERT_NO_THROW(pieceAt("A","B"));
+}
+
+// Testing gameState
